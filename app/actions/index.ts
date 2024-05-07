e135fcc93c0a885b1a9acce596ad4b75f65d3fda
Matheiu Perdido <matheiuamiel@gmail.com>
2024-05-07 23:49:28 +0800

    edit login redirect

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -22,7 +22,7 @@ export async function login(formData: FormData) {
   }
 
   revalidatePath("/", "layout");
-  redirect("/account");
+  redirect("/home");
 }
 
 export async function signup(formData: FormData) {
